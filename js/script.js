4dbcb1187c86a7b6ba2fa4b0584827a66fb642e5
abby-shabi <abbyshabi@gmail.com>
2018-12-14 13:00:34 +0300

    added the begin function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
+function begin() {
 
+    // ask users to enter name
+    playerNum1 = prompt("Player One, Enter Your Name:");
+    playerNum2 = prompt("Player Two, Enter Your Name:");
 
+    // ask users to enter name again if failed the first time
+    if (playerNum1 == "" || playerNum2 == "" || playerNum1 == null || playerNum2 == null) {
+        alert("Please Enter Your Names Players One and Two!");
+        location.reload();
+    } else {
+        document.getElementById("name-0").innerHTML = " " + playerNum1;
+        document.getElementById("name-1").innerHTML = " " + playerNum2;
+        //document.getElementById("action").innerHTML = " " + playerNum1;
+        //document.getElementById("scorerOne").innerHTML = " " + playerOneScore;
+        //document.getElementById("scorerTwo").innerHTML = " " + playerTwoScore;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
