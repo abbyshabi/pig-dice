e5023621855d958af602f9a1ab80c4452a012fce
abby-shabi <abbyshabi@gmail.com>
2018-12-14 12:57:24 +0300

    added jquery function to hide/show

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $("#game").hide();
+   
+   })
+   $(document).ready(function(){
+   $("#next").click(function(){
+     $("#game").show();
+        $("#first") . hide();
+   })
+})
+
+
+
+
