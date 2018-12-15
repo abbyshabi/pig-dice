be5c3cddc91fbee0bbd99cebc3c46c416f0af1f0
abby-shabi <abbyshabi@gmail.com>
2018-12-15 22:39:59 +0300

    added dice images

--- js/script.js
+++ js/script.js
@@ -5,7 +5,23 @@ var playerNum1,
     scoreOne = 0;
     scoreTwo = 0;
     player = 0;
-    //currentPlayer = 0;
+
+  var face1=new Image()
+  face1.src="d1.gif"
+  var face2=new Image()
+  face2.src="d2.gif"
+  var face3=new Image()
+  face3.src="d3.gif"
+  var face4=new Image()
+  face4.src="d4.gif"
+  var face5=new Image()
+  face5.src="d5.gif"
+  var face6=new Image()
+  face6.src="d6.gif"
+
+
+
+    
 
 function begin() {
 
@@ -30,7 +46,10 @@ function dice(){
   
     number = Math.ceil(Math.random() * 6);
 
-}
+     document.images["mydice"].src=eval("face"+ number +".src") 
+    }
+
+
 function roll() {
     
         
@@ -41,40 +60,50 @@ function roll() {
         }
 
         else if (number = 1) {
+
             score = 0;
             document.getElementById("message").value = "sorry you rolled a one and lost your turn"
         }
         else{
-           score = 0;
+              document.getElementById("result").innerHTML = " " + score;
         }
         event.preventDefault();
 }
 
 function passOn(){
 
-       
+     
     if (player == 0){
 
         scoreOne = scoreOne + score;
         document.getElementById("score1").value = " " + scoreOne;
-        document.getElementById("message").value = "it's now " + playerNum2 + "'s turn" 
+        alert(" it's now " + playerNum2 + "'s turn")
+        //document.getElementById("message").value = "it's now " + playerNum2 + "'s turn" 
       
             player = 1;
-
+            if (scoreOne >= 100){
+              alert (playerNum1 + " is the winner!!!! with a score of : " + scoreOne)
+            }
+           
+                score = 0;
     }
     
-   else if  (player == 1){
-        number = 0;
+   else if  (player == 1) {
+          //if ( score = 0){
+       
          scoreTwo = scoreTwo + score;   
         document.getElementById("score2").value = " " + scoreTwo;
-        document.getElementById("message").value = "it's now " + playerNum1 + "'s turn" 
+        alert(" it's now " + playerNum1 + "'s turn")
+        //document.getElementById("message").value = "it's now " + playerNum1 + "'s turn" 
        
             player = 0;
-
-   }
-
+            if (scoreTwo >= 100){
+              alert (playerNum2 + " is the winner!!!! with a score of : " + scoreTwo)
 }
-
+  
+   }
+      score = 0;
+}
 
 $(document).ready(function(){
     $("#game, #maze").hide();
@@ -106,3 +135,9 @@ $(document).ready(function(){
         $("#game,#maze") . hide();
    })
 })
+   $(document).ready(function(){
+   $(":reset").click(function(){
+            
+     })
+ })
+
